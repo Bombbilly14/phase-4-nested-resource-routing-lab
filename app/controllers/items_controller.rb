154e9d90b02488a14ddb1f8c402b69762390ab84
William German <william.j.german2@gmail.com>
2023-01-12 15:37:52 -0800

    commit tree 73a9d9107e7ed4c0a4fe2a7d456feaa37ba491bd

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,48 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
   def index
-    items = Item.all
+
+    user_id = params[:user_id]
+    items = user_id ? User.find(user_id).items : Item.all
+    # if params[:user_id]
+    #   items = User.find(params[:user_id]).items
+    #   if user
+    #   items = user.items
+    #   else
+    #     return render json: {"message": "User not found"}, status: :not_found
+    # else
+    # items = Item.all
+    # end
     render json: items, include: :user
+  end
+
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def record_not_found
+    render json: {error: "User not found"}, status: :not_found
+
+  end
+
+  def record_invalid(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # /items/ (line 5)
+  # /user/:user_id (line 6)
+  # but we want /user/:user_id/items (line 7)
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :items, only: [:index,  :show, :create]
+  end
 end
